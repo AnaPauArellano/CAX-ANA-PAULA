d61c4a1814b4de0ce6cd28fbd08a6f0ab24402a7
AnaPauArellano <104459832+AnaPauArellano@users.noreply.github.com>
2022-05-25 13:51:16 -0500

    Add files via upload

---
+++ Bloque 1/October 6-2.py
@@ -0,0 +1,24 @@
+nombre='Ana Pau'
+print('Hola mi nombre  es', nombre)
+grado='noveno grado'
+print('Voy en ', grado)
+grupo='A'
+print('Mi grupo es el ', grupo)
+comida='pizza'
+print('Mi comida favorita es ', comida)
+mensaje='Vive en el presente, no en el pasado'
+print('Mi mensaje es ', mensaje)
+
+
+
+nombre='Domenica'
+print('Hola mi nombre  es', nombre)
+grado='noveno grado'
+print('Voy en ', grado)
+grupo='A'
+print('Mi grupo es el ', grupo)
+comida='pizza'
+print('Mi comida favorita es ', comida)
+mensaje='Tengo hambre'
+print('Mi mensaje es ', mensaje)
+
