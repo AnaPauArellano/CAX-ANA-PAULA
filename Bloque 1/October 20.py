0af5fa25f4912dedd240c238b5498d1bca1e92ae
AnaPauArellano <104459832+AnaPauArellano@users.noreply.github.com>
2022-05-25 13:53:31 -0500

    Add files via upload

---
+++ Bloque 1/October 20.py
@@ -0,0 +1,11 @@
+print("How can we act with empathy and positivity when we are online?")
+
+print("Be kind and help people but without favoritism or not being always in favor of that person")
+
+print("Identify: the boy, the girl and the best friend of the girl; the boy had the dilemma to deliver or not the letter; the girl to send or not the message; the best friend to public or not something private")
+
+print("Feel: the boy felt in love and scare, the girl felt funny and inocent, the best friend cruel and embarassed the boy; it was difficult for the boy but not for the girls")
+
+print("Imagine: if the boy didnt deliver the letter, the girl didnt have send the letter to her best friend; if the girl didnt send the photo of the letter, the friend havent public the photo; if the friend didnt public the letter and feel like bad for the boy , the boy didnt have to suffer from bullying")
+
+print("Say: if the boy hadnt deliver the letter, he would be sad, the girl wouldnt even notice the boy feelings and the friend wouldnt know anything about it")
