40bb86a751b503521eaafa3e55d20e4a1a328eb0
AnaPauArellano <104459832+AnaPauArellano@users.noreply.github.com>
2022-05-25 13:49:50 -0500

    Add files via upload

---
+++ October 6-1.py
@@ -0,0 +1,12 @@
+x=5009765
+
+jamon=23
+
+reprobados='9b'
+
+
+print('todo el grupo A de noveno esta reprobado')
+
+print('este grupo esta reprobado: ' , reprobados)
+print('el valor de x es: ', x)
+print('el precio del jamon es: ', jamon)
