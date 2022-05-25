294d0812ac7f782ce4dc1f187ae74eb6576c664b
AnaPauArellano <104459832+AnaPauArellano@users.noreply.github.com>
2022-05-25 13:55:48 -0500

    Add files via upload

---
+++ Bloque 1/November 3-3.py
@@ -0,0 +1,29 @@
+name=input("what is your username?")
+print("hi",name)
+
+place=input("where are you from?")
+print("I love", place)
+
+likes=input("what do you like?")
+print("Me too")
+
+color=input("what is your favorite color?")
+print("I love that color")
+
+food=input("what is your favorite food?")
+print("That is my favorite food too")
+
+travel=input("where would you like to travel?")
+print("That is a beautiful place")
+
+dreams=input("what are your dreams?")
+print("Beautiful dream")
+
+study=input("what would you like to study?")
+print("wow, you want to study", study,"i know you are gonna make it")
+
+christmas=input("what do you like about christmas?")
+print("Wonderful")
+
+message=input("what is your message?")
+print("Beautiful message")
