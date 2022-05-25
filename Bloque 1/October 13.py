b083d81cea57b0ddb6ee4f118eb6a326ec10c5d7
AnaPauArellano <104459832+AnaPauArellano@users.noreply.github.com>
2022-05-25 13:52:11 -0500

    Add files via upload

---
+++ Bloque 1/October 13.py
@@ -0,0 +1,31 @@
+trombones76 = 'big parade'
+more = 1000
+clas = 'advanced theorical Zymurgy'
+
+
+
+hola = 123
+reprobados = 234
+novenoa = 345
+
+
+
+suma=hola+reprobados+novenoa
+print(suma)
+
+
+
+resta=reprobados-novenoa
+print(resta)
+
+
+multiplicacion=hola*novenoa
+print(multiplicacion)
+
+
+div=reprobados/hola
+print(div)
+
+
+ptencia=reprobados**2
+print(ptencia)
