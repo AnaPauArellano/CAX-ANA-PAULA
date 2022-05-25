e95c49f1e4a7af006076acac3ffe991bf9816b28
AnaPauArellano <104459832+AnaPauArellano@users.noreply.github.com>
2022-05-25 13:55:16 -0500

    Add files via upload

---
+++ Bloque 1/November 3-2.py
@@ -0,0 +1,12 @@
+#realizar el codigo de la primera sesion en forma de preguntas y contestarlo usando input
+
+print('hola')
+nombre=input('como te llamas')
+
+grado=input('cual es tu grado ')
+
+grupo=input('en que grupo vas')
+
+comidafav=input('cual es tu comida favorita')
+
+algoqdecir=input('cual es tu mensaje')
